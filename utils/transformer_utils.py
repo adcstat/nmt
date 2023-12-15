a0bce4298723f03cf28f0fbe8a20e238523a9a6e
amir <emirhan.temureb.de>
2023-12-15 17:21:17 +0100

    mask = mask.to(source_query.device)

--- utils/transformer_utils.py
+++ utils/transformer_utils.py
@@ -131,6 +131,7 @@ class Attention(nn.Module):
         # autoregressive masking only makes sense for source_query == source_key_value
         if self.masked:
             mask = torch.tril(torch.ones(attention_weights.shape[1], attention_weights.shape[1]))
+            mask = mask.to(source_query.device)
             attention_weights = attention_weights.masked_fill(mask == 0, float('-inf')) # (batch_size, seq_len_q, seq_len_kv)
         attention_weights = F.softmax(attention_weights, dim=-1) # (batch_size, seq_len_q, seq_len_kv)
         attention_weights = self.dropout(attention_weights)
