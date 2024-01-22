d9213e9d7b7b8b2b0de9c3581c33893a57c85ef4
amir <emirhan.temureb.de>
2024-01-22 08:40:35 +0100

    simplify

--- collect_losses.py
+++ collect_losses.py
@@ -2,10 +2,10 @@ import argparse
 import numpy as np
 import torch
 
-def collect_losses(checkpoint_path, num_checkpoints):
+def collect_losses(checkpoint_path, epochs):
     train_losses = []
     val_losses = []
-    for i in range(1, num_checkpoints + 1):
+    for i in range(1, epochs + 1):
         checkpoint = torch.load(f"{checkpoint_path}/checkpoint_{i}.tar")
         train_losses.append(checkpoint["TRAIN_LOSSES"])
         val_losses.append(checkpoint["VAL_LOSS"])
@@ -19,10 +19,10 @@ def collect_losses(checkpoint_path, num_checkpoints):
 
 def main():
     parser = argparse.ArgumentParser(description="Load checkpoints and save loss arrays.")
-    parser.add_argument("checkpoint_path", type=str, help="Path to the checkpoint directory")
-    parser.add_argument("number_of_checkpoints", type=int, help="Number of checkpoints to process")
+    parser.add_argument("path", type=str, help="Path to the checkpoint directory")
+    parser.add_argument("epochs", type=int, help="Number of checkpoints to process")
     args = parser.parse_args()
-    collect_losses(args.checkpoint_path, args.number_of_checkpoints)
+    collect_losses(args.path, args.epochs)
 
 if __name__ == "__main__":
     main()
