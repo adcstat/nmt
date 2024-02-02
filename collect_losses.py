a7d4270cb7097094053b66581598dd2b39446a7e
adcstat <emirhan.temur@web.de>
2024-02-02 11:15:59 +0000

    better loss collecting

--- collect_losses.py
+++ collect_losses.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 import os
 import numpy as np
 import torch
@@ -8,11 +9,28 @@ def collect_losses(losses_path):
     val_losses = []
     fps = [os.path.join(losses_path, fn) for fn in os.listdir(losses_path) if fn.endswith(".tar")]
     fps.sort()
+
+    # Group files by checkpoint (excluding the last part after the last underscore)
+    checkpoint_groups = defaultdict(list)
     for fp in fps:
-        checkpoint = torch.load(fp)
-        train_losses.extend(np.array(checkpoint["TRAIN_LOSSES"]).T.mean(-1))
-        val_losses.append(checkpoint["VAL_LOSSES"])
-        print(f"processed {fp}")
+        base_name = os.path.basename(fp)
+        checkpoint_key = "_".join(base_name.split("_")[:-1])
+        checkpoint_groups[checkpoint_key].append(fp)
+    
+    for checkpoint_key, files in checkpoint_groups.items():
+        temp_val_losses = []
+        for fp in files:
+            checkpoint = torch.load(fp)
+            train_losses.extend(np.array(checkpoint["TRAIN_LOSSES"]).T.mean(-1))
+            temp_val_losses.append(checkpoint["VAL_LOSSES"])
+            print(f"processed {fp}")
+        # Average val losses for checkpoints with multiple files
+        if len(temp_val_losses) > 1:
+            avg_val_loss = np.mean(temp_val_losses, axis=0)
+            val_losses.append(avg_val_loss)
+        else:
+            val_losses.extend(temp_val_losses)
+
     train_losses = np.array(train_losses)
     val_losses = np.array(val_losses)
 
